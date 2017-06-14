5d4b67bf7626b3a056a17637a4eeb02467c08269
Viktors Oginskis <viktors.oginskis@accenture.com>
2017-06-14 13:49:12 +0200

    added error handling for flat persisting failures

--- app/actor/PersistActor.scala
+++ app/actor/PersistActor.scala
@@ -1,6 +1,7 @@
 package actor
 
 import akka.actor.{ActorLogging, Actor, ActorRef}
+import com.mongodb.MongoCommandException
 import model.b2c.Flat
 import play.api.Logger
 import repo.FlatRepo
@@ -12,16 +13,25 @@ class PersistActor(notificationActor: ActorRef, flatRepo: FlatRepo) extends Acto
 
   override def receive: Receive = {
     case flat: Flat => {
-      val flatStatus = flatRepo.addOrUpdateFlat(flat)
-      def matchesFilter(flat: Flat): Boolean = {
-        if (flat.price.get < 90000
-          && flat.size.get >= 40
-        ) true
-        else false
+      try {
+        val flatStatus = flatRepo.addOrUpdateFlat(flat)
+        def matchesFilter(flat: Flat): Boolean = {
+          if (flat.price.get < 90000
+            && flat.size.get >= 40
+          ) true
+          else false
+        }
+        if (Flat.Added == flatStatus && matchesFilter(flat)) {
+          Logger.info(s"New flat has been found $flat Sending out emails...")
+          notificationActor ! flat
+        }
       }
-      if (Flat.Added == flatStatus && matchesFilter(flat)) {
-        Logger.info(s"New flat has been found $flat Sending out emails...")
-        notificationActor ! flat
+      catch {
+        case e: MongoCommandException => {
+          val message = e.getErrorMessage
+          Logger.info(s"$flat flat insert/update has failed with $message, will retry")
+          self ! flat
+        }
       }
     }
   }
